7b4c714aeae3f4f4d2ac19dd4212fb7329ee3ad1
jeonminseo <xx0419xx@naver.com>
2022-03-31 16:34:48 +0900

    string bactor &count

---
+++ problem 77.py
@@ -0,0 +1,9 @@
+mystr ="Good NIGHT"
+
+#[a:b] b-1까지 나옴
+print(mystr[1:3])
+print(mystr[4:9])
+
+#항상 작은수 : 큰 수
+print(mystr[-3:-1])
+print(mystr[2:4])
---
+++ problem 81.py
@@ -0,0 +1,4 @@
+#공백없이 문자열 갯수 출력
+string="가나다라 마 바 사아자 차카 타"
+
+print(len(string)-string.count(" "))
